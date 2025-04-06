fce8f56615cc88ec08a75b52d27919326a7e2a26
alifsyr <alifsyr@gmail.com>
2025-04-06 17:54:13 +0700

    fix: using lombok getter for LoginResponseDto

--- src/main/java/com/odp/walled/dto/auth/LoginResponseDto.java
+++ src/main/java/com/odp/walled/dto/auth/LoginResponseDto.java
@@ -1,35 +1,17 @@
 package com.odp.walled.dto.auth;
 
-public class LoginResponseDto {
-    // private String token;
+import lombok.Getter;
 
-    //new 
+@Getter
+public class LoginResponseDto {
     private String accessToken;
     private String refreshToken;
 
     private long expiresIn;
 
-    // public String getToken() {
-    //     return token;
-    // }
-
-    // public LoginResponse setToken(String token) {
-    //     this.token = token;
-    //     return this;
-    // }
-
-    //new
-    public String getAccessToken() {
-        return accessToken;
-    }
-
     public LoginResponseDto setAccessToken(String accessToken) {
         this.accessToken = accessToken;
         return this;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
     }
 
     public LoginResponseDto setRefreshToken(String refreshToken) {
@@ -37,24 +19,11 @@ public class LoginResponseDto {
         return this;
     }
 
-    public long getExpiresIn() {
-        return expiresIn;
-    }
-
     public LoginResponseDto setExpiresIn(long expiresIn) {
         this.expiresIn = expiresIn;
         return this;
     }
 
-    // @Override
-    // public String toString() {
-    //     return "LoginResponse{" +
-    //             "token='" + token + '\'' +
-    //             ", expiresIn=" + expiresIn +
-    //             '}';
-    // }
-
-    //new
     @Override
     public String toString() {
         return "LoginResponse{" +
